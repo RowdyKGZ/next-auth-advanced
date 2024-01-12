7a7f0cb96389567946ace59bf36f762e01d14b2f
RowdyKGZ <rowdykg@gmail.com>
2024-01-12 17:28:53 +0600

    card wrapper

---
+++ components/auth/header.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({ subsets: ["latin"], weight: ["600"] });
+
+interface HeaderProp {
+  label: string;
+}
+
+export const Header = ({ label }: HeaderProp) => {
+  return (
+    <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+      <h1 className={cn("text-3xl font-semibold", font.className)}>🔒Auth</h1>
+      <p className="text-muted-foreground text-sm">{label}</p>
+    </div>
+  );
+};
