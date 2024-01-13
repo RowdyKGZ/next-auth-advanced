f3c3ebcf6b8e1af60160fb1d585dec324a6a5440
RowdyKGZ <rowdykg@gmail.com>
2024-01-13 23:31:54 +0600

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -11,5 +14,20 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { password, email, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: { name, email, password: hashedPassword },
+  });
+
+  // TODO: Send verification email
+
+  return { success: "User created" };
 };
---
+++ data/user.ts
@@ -0,0 +1,17 @@
+import { db } from "@/lib/db";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { email } });
+  } catch {
+    return null;
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } });
+  } catch {
+    return null;
+  }
+};
